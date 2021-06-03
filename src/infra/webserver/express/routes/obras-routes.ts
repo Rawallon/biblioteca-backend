4d2302c9982311eb22cc6cf99a70a63fdde593f0
rawallon <rawallon@gmail.com>
2021-06-03 13:48:31 -0300

    Added put route on express

--- src/infra/webserver/express/routes/obras-routes.ts
+++ src/infra/webserver/express/routes/obras-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { makeAddBookController } from '../../../db/factories/make-add-book'
 import { makeGetBooksController } from '../../../db/factories/make-get-books-controller'
+import { makePutBookController } from '../../../db/factories/make-put-book'
 import { adaptRoute } from '../adapter/route-adpter'
 
 export default function obrasRoute (router: Router): void {
   router.get('/obras', adaptRoute(makeGetBooksController()))
   router.post('/obras', adaptRoute(makeAddBookController()))
+  router.put('/obras/:id', adaptRoute(makePutBookController()))
 }

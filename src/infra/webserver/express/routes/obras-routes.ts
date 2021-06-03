c7e303b883f88ec993c90ca69e248c2374fe5562
rawallon <rawallon@gmail.com>
2021-06-03 18:04:13 -0300

    Implemented Delete route to Express

--- src/infra/webserver/express/routes/obras-routes.ts
+++ src/infra/webserver/express/routes/obras-routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { makeAddBookController } from '../../../db/factories/make-add-book'
+import { makeDeleteBookController } from '../../../db/factories/make-delete-book'
 import { makeGetBooksController } from '../../../db/factories/make-get-books-controller'
 import { makePutBookController } from '../../../db/factories/make-put-book'
 import { adaptRoute } from '../adapter/route-adpter'
@@ -8,4 +9,5 @@ export default function obrasRoute (router: Router): void {
   router.get('/obras', adaptRoute(makeGetBooksController()))
   router.post('/obras', adaptRoute(makeAddBookController()))
   router.put('/obras/:id', adaptRoute(makePutBookController()))
+  router.delete('/obras/:id', adaptRoute(makeDeleteBookController()))
 }
